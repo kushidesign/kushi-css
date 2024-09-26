b68cb1be217d9dc5f6e8ea966c6c3588e6784a06
Jeremiah Coyle <jcoyle@gmail.com>
2024-09-25 18:14:13 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kushi-css "0.1.0-SNAPSHOT"
+(defproject design.kushi/kushi-css "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -13,7 +13,7 @@
                                   ;;  for local testing
                                   "../humane-test-output-master/src"
                                   ]
-                   :dependencies [[io.github.paintparty/fireworks "0.6.2"]
+                   :dependencies [[io.github.paintparty/fireworks "0.6.4"]
                                   [io.github.paintparty/bling "0.1.1"]
                                   ;; for testing
                                   [com.taoensso/tufte "2.6.3"]
